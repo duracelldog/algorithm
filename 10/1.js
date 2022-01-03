7a3bc6c053f841a4e08cc5991f0483d57988420a
신범근 <tlsqjarms@naver.com>
2022-01-03 22:28:45 +0900

    290p

--- 10/1.js
+++ 10/1.js
@@ -1,56 +1,82 @@
-
-
-// const union_parent = (parent, a, b) => {
-//   const a
-// }
-
 const v = 6; // 노드의 개수
 const e = 4; // 간선의 개수
 const 간선정보 = [
   [1, 4],
   [2, 3],
   [2, 4],
-  [5, 6]
-]
+  [5, 6],
+];
 
-const parent = Array.from({ length: v + 1 }, () => 0);
+const parents = Array.from({ length: v + 1 }, (_, k) => k);
 
-// 일반
-// const find_parent = (x) => {
-//   if (parent[x] !== x) {
-//     return find_parent(parent[x]);
-//   }
-
-//   return x;
-// }
-
-// 경로 압축 기법
-const find_parent = (x) => {
-  if (parent[x] !== x) {
-    parent[x] = find_parent(parent[x]);
+const findParent = (x) => {
+  if (parents[x] === x) {
+    return x;
   }
 
-  return parent[x];
-}
+  return findParent(parents[x]);
+};
 
-for(let i=1; i<v+1; i++) {
-  // 1~6
-  parent[i] = i
-}
+간선정보.forEach(([node1, node2]) => {
+  const parent1 = findParent(node1);
+  const parent2 = findParent(node2);
 
-// [0, 1, 2, 3, 4, 5, 6]
-
-간선정보.forEach(val => {
-  val[0] = find_parent(val[0]);
-  val[1] = find_parent(val[1]);
-
-  if (val[0] < val[1]) {
-    parent[val[1]] = val[0];
+  if (parent1 < parent2) {
+    parents[parent2] = parent1;
   } else {
-    parent[val[0]] = val[1];
+    parents[parent1] = parent2;
   }
 });
 
-const endPoint = parent.map(val => find_parent(val));
+console.log({ parents });
 
-console.log({ parent, endPoint })
+// const v = 6; // 노드의 개수
+// const e = 4; // 간선의 개수
+// const 간선정보 = [
+//   [1, 4],
+//   [2, 3],
+//   [2, 4],
+//   [5, 6],
+// ];
+
+// const parent = Array.from({ length: v + 1 }, () => 0);
+
+// // 일반
+// // const find_parent = (x) => {
+// //   if (parent[x] !== x) {
+// //     return find_parent(parent[x]);
+// //   }
+
+// //   return x;
+// // }
+
+// // 경로 압축 기법
+// const find_parent = (x) => {
+//   if (parent[x] !== x) {
+//     parent[x] = find_parent(parent[x]);
+//   }
+
+//   return parent[x];
+// };
+
+// for (let i = 1; i < v + 1; i++) {
+//   // 1~6
+//   parent[i] = i;
+// }
+
+// // [0, 1, 2, 3, 4, 5, 6]
+
+// 간선정보.forEach((val) => {
+//   val[0] = find_parent(val[0]);
+//   val[1] = find_parent(val[1]);
+
+//   if (val[0] < val[1]) {
+//     parent[val[1]] = val[0];
+//   } else {
+//     parent[val[0]] = val[1];
+//   }
+// });
+
+// const endPoint = parent.map((val) => find_parent(val));
+
+// console.log({ parent, endPoint });
--- 10/5.js
+++ 10/5.js
@@ -1,6 +1,6 @@
+// 크루스칼 알고리즘 (최소 신장 트리)
 const v = 7; // 노드의 개수
 const e = 9; // 간선의 개수
-const parent = Array.from({ length: v + 1 }, () => 0);
 const edges = [
   [1, 2, 29],
   [1, 5, 75],
@@ -12,43 +12,72 @@ const edges = [
   [5, 6, 53],
   [6, 7, 25],
 ];
+const parents = Array.from({ length: v + 1 }, (_, k) => k);
 let result = 0;
 
-const union_parent = (a, b) => {
-  a = find_parent(a);
-  b = find_parent(b);
+const union = (node1, node2) => {
+  const parentA = findParent(node1);
+  const parentB = findParent(node2);
 
-  if (a < b) {
-    parent[b] = a;
+  if (parentA < parentB) {
+    parents[parentB] = parentA
   } else {
-    parent[a] = b;
+    parents[parentA] = parentB
   }
 };
 
-const find_parent = (x) => {
-  if (parent[x] !== x) {
-    parent[x] = find_parent(parent[x]);
+const findParent = (node) => {
+  if (parents[node] !== node) {
+    parents[node] = findParent(parents[node]);
   }
 
-  return parent[x];
-};
-
-for (let i = 1; i < v + 1; i++) {
-  // 1~7
-  parent[i] = i;
+  return parents[node];
 }
 
-// console.log({ edges });
-edges.sort((a, b) => a[2] - b[2]);
-// console.log({ edges });
+const sortedEdges = edges.sort((a, b) => a[2] - b[2]);
 
-for (let edge of edges) {
-  const [a, b, cost] = edge;
-
-  if (find_parent(a) !== find_parent(b)) {
-    union_parent(a, b);
+sortedEdges.forEach(([node1, node2, cost]) => {
+  if (findParent(node1) !== findParent(node2)) {
+    union(node1, node2);
     result += cost;
   }
-}
+})
 
-console.log({ result });
+// const union_parent = (a, b) => {
+//   a = find_parent(a);
+//   b = find_parent(b);
+
+//   if (a < b) {
+//     parent[b] = a;
+//   } else {
+//     parent[a] = b;
+//   }
+// };
+
+// const find_parent = (x) => {
+//   if (parent[x] !== x) {
+//     parent[x] = find_parent(parent[x]);
+//   }
+
+//   return parent[x];
+// };
+
+// for (let i = 1; i < v + 1; i++) {
+//   // 1~7
+//   parent[i] = i;
+// }
+
+// // console.log({ edges });
+// edges.sort((a, b) => a[2] - b[2]);
+// // console.log({ edges });
+
+// for (let edge of edges) {
+//   const [a, b, cost] = edge;
+
+//   if (find_parent(a) !== find_parent(b)) {
+//     union_parent(a, b);
+//     result += cost;
+//   }
+// }
+
+// console.log({ result });
